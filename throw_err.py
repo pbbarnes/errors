16cda88a16a9362fd6fe6c46b3e8a36127252075
Paul Barnes <barnesp@hawaii.edu>
2015-09-22 18:23:49 -1000

    new errors

--- throw_err.py
+++ throw_err.py
@@ -3,15 +3,20 @@
 # This script raises an error based on 
 # user-supplied command line argument
 
+import math
 import sys
+import os
+import itertools
 
 def print_usage():
+    
     """Print usage and exit"""
     sys.stderr.write("usage: python raise_err.py <error type>\n")
     sys.stderr.write("available errors: \n")
     sys.stderr.write("\tassertion, io, import, index\n")
     sys.stderr.write("\tkey, name, os, type, value,\n")
-    sys.stderr.write("\tzerodivision\n")
+    sys.stderr.write("\tzerodivision, syntax, StopIteration, \n")
+    sys.stderr.write("\tkeyboard, GeneratorExit\n")
     sys.exit()
 
 # Check args
@@ -21,25 +26,67 @@ if len(sys.argv) != 2:
 error_type = sys.argv[1]
 
 if error_type == "assertion":
-    raise AssertionError
+    f = 3
+    assert f== 4
 elif error_type == "io":
-    raise IOError
+    f = open('myfile.txt')
 elif error_type == "import":
-    raise ImportError
+    import Config as CF
 elif error_type == "index":
-    raise IndexError
+    spam = ['cat', 'dog', 'mouse']
+    print(spam[6])
 elif error_type == "key":
-    raise KeyError
+    spam = {'cat': 'Kit', 'dog': 'Dawg'}
+    print('The name of my pet giraffe is' + spam['zebra'])
 elif error_type == "name":
-    raise NameError
+    hoopla = 'Paul'
+    print('My name is ' + hoopl)
 elif error_type == "os":
-    raise OSError
+   for i in range(10):
+       print i, os.ttyname(i)
 elif error_type == "type":
-    raise TypeError
+    x = '2' + 2
 elif error_type == "value":
-    raise ValueError
+    x = int(input("Please enter a number: "))
 elif error_type == "zerodivision":
-    raise ZeroDivisionError
+    x = 1/0
+elif error_type == "StopIteration":
+    l = [0, 1, 2]
+    i = iter(l)
+    print i
+    print i.next()
+    print i.next()
+    print i.next()
+    print i.next()
+elif error_type == "GeneratorExit":
+    def my_generator():
+        try:
+            for i in range(5):
+                print 'Yielding', i
+                yield i
+        except GeneratorExit:
+             print 'Exiting Early'
+
+    g = my_generator()
+    print g.next()
+    g.close()
+elif error_type == "keyboard":
+    try:
+        print 'Press Retuen or Ctrl-C:',
+        ignored = raw_input()
+    except Exception, err:
+        print 'Caught exception:', err
+    except KeyboardInterrupt, err:
+        print 'Caught KeyboardInterrupt'
+    else:
+        print 'No exception'
+elif error_type == "syntax":
+    try:
+        print eval('five times three')
+    except SyntaxError, err:
+        print 'Syntax error %s (%s-%s): %s' % \
+            (err.filename, err.lineno, err.offset, err.text)
+        print err
 else:
     sys.stderr.write("Sorry, not able to throw a(n) ")
     sys.stderr.write(error_type + " error\n")
